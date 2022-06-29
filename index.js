a0b90759f234c006d1401f205e33f6869c04c8f9
Derik McCausland <derikmccausland@gmail.com>
2022-06-29 15:28:54 -0600

    finished

---
+++ index.js
@@ -0,0 +1,46 @@
+//STEP 1: 
+const formData = document.querySelector("form")
+const message = document.querySelector('#message')
+
+//STEP 2:
+function addMovie (event) {
+    event.preventDefault()
+    let inputField = document.querySelector('input') 
+    const movie = document.createElement('li')
+    const movieTitle = document.createElement('span')
+    movieTitle.addEventListener('click', crossOffMovie)
+    movieTitle.textContent = inputField.value
+    movie.appendChild(movieTitle)
+    
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = ('x')
+    deleteBtn.addEventListener('click', deleteMovie)
+    movie.appendChild(deleteBtn)
+    
+    
+    const list = document.querySelector('ul')
+    list.appendChild(movie)  
+
+    inputField.value = ""
+    message.textContent = 'Movie added'
+}
+
+function deleteMovie (event) {
+    event.target.parentNode.remove()
+    message.textContent = 'Movie deleted'
+}
+
+function crossOffMovie (event) {
+    event.target.classList.toggle('checked')
+    if (event.target.classList.contains ('checked')){
+        message.textContent = 'Watched'
+    }
+    else{
+     message.textContent = 'Re-added movie'
+    }
+    
+}
+
+//STEP 3:
+formData.addEventListener('submit', addMovie)
+
